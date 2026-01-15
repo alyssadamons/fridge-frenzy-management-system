2b069f54f7896be0d5d281ee44e7458cc936f005
alyssadamons <alyssadamons3@gmail.com>
2026-01-15 09:31:12 +0200

    Add Client

---
+++ Client/PayPalClient.cs
@@ -0,0 +1,16 @@
+﻿namespace E_Commerce.Client
+{
+    public class PayPalClient
+    {
+        public string Mode { get; }
+        public string ClientId { get; }
+        public string ClientSecret { get; }
+        public string BaseUrl => Mode == "Live" ? "https://api.paypal.com" : "https://api.sandbox.paypal.com";
+        public PayPalClient(string mode, string clientId, string clientSecret)
+        {
+            Mode = mode ?? "Sandbox";
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+        }
+    }
+}
